a91861ba675aa97660f7752dfde3e043cb018d8e
Michael Mafeni <michael.mafeni@bbc.co.uk>
2024-12-12 18:52:32 +0000

    WIP Day 9 part 2

--- 09/main.ts
+++ 09/main.ts
@@ -42,6 +42,46 @@ export function reorder(arr: number[]): number[] {
   return arr;
 }
 
+function swapWithFreeSpaceBlock(arr: number[], blockLength: number, blockStart: number) {
+  for (let i = 0; i < blockStart; i++) {
+    if (isFreeSpace(arr[i])) {
+      let freeSpaceEnd = i;
+      for (let j = i + 1; j < blockStart; j++) {
+        if (!isFreeSpace(arr[j])) {
+          freeSpaceEnd = j;
+          break;
+        }
+      }
+      const freeSpaceBlock = arr.slice(i, freeSpaceEnd);
+      if (freeSpaceBlock.length >= blockLength) {
+        // Do the swaps
+        for (let k = 0; k < blockLength; k++) {
+          const temp = arr[blockStart + k];
+          arr[blockStart + k] = arr[i + k];
+          arr[i + k] = temp;
+        }
+        break;
+      }
+    }
+  } 
+}
+
+function reorder2(arr: number[]): number[] {
+  for (let i = arr.length - 1; i >= 0; i--) {
+    const value = arr[i];
+    if (isFile(value)) {
+      let start = i;
+      while(arr[start] === arr[i]) {
+        start--;
+      }
+      const block = arr.slice(start + 1, i + 1);
+      swapWithFreeSpaceBlock(arr, block.length, start+1);
+    }
+  }
+
+  return arr;
+}
+
 function validList(list: number[]): boolean {
   const freeSpaceCount = list.filter((x) => x === -1).length;
   const index = list.length - freeSpaceCount;
@@ -63,10 +103,14 @@ function calcCheckSum(reordered: number[]): number {
 }
 
 if (import.meta.main) {
-  const text = await Deno.readTextFile(import.meta.dirname + "/input.txt");
+  const text = await Deno.readTextFile(import.meta.dirname + "/test.txt");
   const decompressed = decompress(text);
   const reordered = reorder(decompressed);
   const checkSum = calcCheckSum(reordered);
 
   console.log("Part 1:", checkSum);
+  const reordered2 = reorder2(decompress(text));
+  console.log(reordered2);
+  const checkSum2 = calcCheckSum(reordered2);
+  console.log("Part 2:", checkSum2);
 }
